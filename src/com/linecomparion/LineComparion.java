ee527ee4c6c75224f4afd29294b914420de46bd3
divyanaC999 <divyachourasiya999@gmail.com>
2022-02-14 19:55:53 +0530

    first commit

---
+++ src/com/linecomparion/LineComparion.java
@@ -0,0 +1,11 @@
+package com.linecomparion;
+
+public class LineComparion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Line Comparion Computation Program");
+
+	}
+
+}
